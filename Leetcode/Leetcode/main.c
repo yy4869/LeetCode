9a42ffbc1ab5fbf98c308b3b6f1a6b484b76b8fd
yaoyuan01 <yaoyuan01@fenbi.com>
2020-08-23 23:09:29 +0800

    ADD: project init

---
+++ Leetcode/Leetcode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Leetcode
+//
+//  Created by 媛小仙女 on 2020/8/23.
+//  Copyright © 2020 yaoyuan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
