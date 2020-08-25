7b57e83b9db114831e4815e0c8bc839e60fcd2ea
yy <yaoyuan01@fenbi.com>
2020-08-25 14:11:06 +0800

    ADD: 10. 正则表达式匹配

--- Leetcode/Leetcode/main.cpp
+++ Leetcode/Leetcode/main.cpp
@@ -8,11 +8,42 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <string>
 
 #include "BitCalculate.hpp"
 
 using namespace std;
 
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        s = " " + s;
+        p = " " + p;
+        int m = s.size(), n = p.size();
+        bool dp[m + 1][n + 1];
+        memset(dp, false, sizeof(dp));
+        dp[0][0] = true;
+
+        for(int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p[j - 1] == '*') {
+                    if (s[i - 1] == p[j - 2] || p[j - 2] == '.') {
+                        dp[i][j] = dp[i - 1][j] || dp[i][j - 1] || dp[i][j - 2];
+                    } else {
+                        dp[i][j] = dp[i][j - 2];
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
+
 int main() {
-    cout << BitCalculate().rangeBitwiseAnd(5, 7) << endl;
+//    cout << BitCalculate().rangeBitwiseAnd(5, 7) << endl;
+    cout << Solution().isMatch("aab", "c*a*b") << endl;;
+    cout << Solution().isMatch("aa", "a");
+    cout << endl;
 }
