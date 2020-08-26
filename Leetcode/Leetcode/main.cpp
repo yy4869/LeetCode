c4687a7eabba833b957d7844f04f2b8cc25a0d28
yy <yaoyuan01@fenbi.com>
2020-08-26 11:50:50 +0800

    ADD: 17. 电话号码的字母组合

--- Leetcode/Leetcode/main.cpp
+++ Leetcode/Leetcode/main.cpp
@@ -9,6 +9,8 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <map>
 
 #include "BitCalculate.hpp"
 
@@ -39,11 +41,60 @@ public:
         }
         return dp[m][n];
     }
+
+
+    map<char, string> digitToStr = {
+        {'2', "abc"},
+        {'3', "def"},
+        {'4', "ghi"},
+        {'5', "jkl"},
+        {'6', "mno"},
+        {'7', "pqrs"},
+        {'8', "tuv"},
+        {'9', "wxyz"},
+    };
+    vector<string> res;
+    vector<string> letterCombinations(string digits) {
+        letterCombination(digits, 0);
+        return res;
+    }
+
+    void letterCombination(string digits, int loc) {
+        if (loc == digits.size()) {
+            return;
+        }
+        char key = digits[loc];
+        string values = digitToStr[key];
+
+        if (loc == 0) {
+            for (int i = 0; i < values.size(); i++) {
+                res.push_back(string(1, values[i]));
+            }
+        } else {
+            vector<string> temp = res;
+            // 扩容n - 1倍
+            for (int i = 0; i < values.size() - 1; i++) {
+                res.insert(res.end(), temp.begin(), temp.end());
+            }
+
+            int time = res.size() / values.size();
+            for (int i = 0; i < values.size(); i++) {
+                for (int j = 0; j < time; j++) {
+                    res[i * time + j] += values[i];
+                }
+            }
+        }
+        letterCombination(digits, loc + 1);
+    }
 };
 
 int main() {
 //    cout << BitCalculate().rangeBitwiseAnd(5, 7) << endl;
-    cout << Solution().isMatch("aab", "c*a*b") << endl;;
-    cout << Solution().isMatch("aa", "a");
+//    cout << Solution().isMatch("aab", "c*a*b") << endl;;
+//    cout << Solution().isMatch("aa", "a");
+    vector<string> res = Solution().letterCombinations("23");
+    for (auto &subStr: res) {
+        cout << subStr << endl;
+    }
     cout << endl;
 }
